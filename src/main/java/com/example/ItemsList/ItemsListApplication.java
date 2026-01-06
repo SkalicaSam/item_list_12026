eb0faefb347e67aa1b17b4ff8611f993b2b995af
Samuel Stehlik <samcostehlik@gmail.com>
2026-01-05 23:02:09 -0800

    first commit

---
+++ src/main/java/com/example/ItemsList/ItemsListApplication.java
@@ -0,0 +1,13 @@
+package com.example.ItemsList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemsListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemsListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ItemsList/ItemsListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ItemsList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemsListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
